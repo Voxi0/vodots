78ab1b12229a10ce0e80d71e4572f08238439e6b
Voxi0 <voxi00@proton.me>
2026-05-02 14:36:56 +0100

    feat: added some extra floorp settings
    not really anything significant or major

--- modules/apps/browsers/floorp.nix
+++ modules/apps/browsers/floorp.nix
@@ -7,12 +7,26 @@
         policies = {
           BlockAboutConfig = false;
         };
+
+        # Extensions and `about:config` settings and such
         profiles."vodots" = {
           isDefault = true;
-          search.default = "ddg";
+          search = {
+            default = "ddg";
+            privateDefault = "ddg";
+          };
 
           # Use Betterfox
           preConfig = "${builtins.readFile ./user.js}";
+
+          # Just some settings
+          settings = {
+            # Don't hide the tab bar when fullscreen
+            "browser.fullscreen.autohide" = false;
+
+            # Stop the alt key from toggling some menu
+            "ui.key.menuAccessKeyFocuses" = false;
+          };
 
           # Extensions
           extensions = {
