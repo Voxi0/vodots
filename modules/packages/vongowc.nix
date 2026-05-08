608d547b84db1089529886b85a44695039415113
Voxi0 <voxi00@proton.me>
2026-05-08 11:09:19 +0100

    feat: configuring mangowc

--- modules/packages/vongowc.nix
+++ modules/packages/vongowc.nix
@@ -1,6 +1,12 @@
-{
-  flake.wrappers.vongowc = {
-    autostart_sh = '''';
-    configFile.path = "";
+{self, ...}: {
+  flake.wrappers.vongowc = {wlib, lib, pkgs, ...}: let
+    system = pkgs.stdenv.hostPlatform.system;
+    voctaliaShell = lib.getExe self.packages.${system}.voctalia-shell;
+  in {
+    autostart_sh = ''
+      ${lib.getExe pkgs.xdg-desktop-portal-wlr}
+      ${voctaliaShell}
+    '';
+    configFile.path = ../../config/mangowc/config.conf;
   };
 }
