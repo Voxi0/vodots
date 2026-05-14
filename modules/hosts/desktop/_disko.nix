1098885c45515c2c68deb93281f9ba4dc6ffa8fa
Voxi0 <voxi00@proton.me>
2026-05-14 16:06:23 +0000

    feat: modified desktop disk layout config
    to better suit impermanence or whatever

--- modules/hosts/desktop/_disko.nix
+++ modules/hosts/desktop/_disko.nix
@@ -1,50 +1,61 @@
 let
-  # System disk where the OS lives
-  primaryDisk = "/dev/sda";
+  systemDrive = "/dev/sda";
+  mountOptions = ["ssd" "noatime" "compress=zstd" "discard=async" "space_cache=v2"];
 in {
+  # Ensure system doesn't boot before "/nix" is mounted
+  fileSystems."/nix".neededForBoot = true;
+
+  # Disk layout
   disko.devices = {
-    # RAM filesystem to mount the root
+    # Store the entire root directory on RAM
+    # So everything gets wiped on every reboot/poweroff whatever
     nodev."/" = {
       fsType = "tmpfs";
       mountOptions = [
-        "size=2G"
+        # Maximum amount of RAM the root directory can use
+        "size=25%"
         "mode=755"
       ];
     };
 
-    # Primary/System disk
-    disk.primary = {
-      device = primaryDisk;
+    # Main/primary disk
+    disk.main = {
+      device = systemDrive;
       type = "disk";
       content = {
         type = "gpt";
         partitions = {
-          # Boot partition
+          boot = {
+            name = "boot";
+            size = "1M";
+            type = "EF02";
+          };
+
           esp = {
-            type = "EF00";
+            name = "ESP";
             size = "1G";
+            type = "EF00";
             content = {
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
-              mountOptions = ["umask=0077"];
             };
           };
 
-          # Root partition
           root = {
+            name = "root";
             size = "100%";
             content = {
               type = "btrfs";
-              extraArgs = [ "-f" ]; # Force overwrite
+              extraArgs = ["-f"];
               subvolumes = {
-                "/persist" = {
-                  mountpoint = "/persist";
-                  mountOptions = ["compress=zstd" "noatime"];
-                };
                 "/nix" = {
+                  inherit mountOptions;
                   mountpoint = "/nix";
-                  mountOptions = ["compress=zstd" "noatime"];
+                };
+                "/persistent" = {
+                  inherit mountOptions;
+                  mountpoint = "/persistent";
                 };
               };
             };
