edd56cdec420413993cd4cf1010a0f2c96eae100
Voxi0 <voxi00@proton.me>
2026-09-15 02:37:40 +0000

    fix: a stupid typo

--- modules/apps/obs-studio.nix
+++ modules/apps/obs-studio.nix
@@ -5,7 +5,7 @@
       plugins = with pkgs.obs-studio-plugins; [
         wlrobs # Allows you to screen capture on wlroots based Wayland compositors
         obs-pipewire-audio-capture # Audio device and application capture for OBS Studio using PipeWire
-        vkcapture # Linux Vulkan/OpenGL game capture
+        obs-vkcapture # Linux Vulkan/OpenGL game capture
         obs-vaapi # VAAPI support via GStreamer
         obs-gstreamer # OBS Studio source, encoder and video filter plugin to use GStreamer elements/pipelines in OBS Studio
       ];
